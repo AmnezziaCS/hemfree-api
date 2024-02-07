6d9ab4dc9542ec0d2204ef3b9047f301e54d7160
Mathys Visticot <mathys.vst@gmail.com>
2024-02-07 16:44:13 +0100

    fix(script): fix pg_reset script

--- scripts/pg_reset.sh
+++ scripts/pg_reset.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Resetting the database..."
+
 # Load environment variables from .env file
 export $(grep -v '^#' .env | xargs)
 
 # Run the pg_reset.sql script on the database
 PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f scripts/sql/pg_reset.sql
+
+echo "Database reset complete."
